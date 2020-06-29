5f9723b826a9b53d5cf1bf04c58e7a739438b6b4
Taisuke Fukuno <fukuno@jig.jp>
2020-06-29 10:30:11 +0900

    add test html on local

--- csv-table.mjs
+++ csv-table.mjs
@@ -5,27 +5,30 @@ class CSVTable extends HTMLElement {
     super();
     const ss = util.decodeCSV(this.textContent.trim());
     this.textContent = "";
+
     const c = tag => document.createElement(tag);
     const tbl = c("table");
+    let firstline = true;
     for (const s of ss) {
       const tr = c("tr");
       tbl.append(tr);
       for (const s3 of s) {
-        const td = c("td");
+        const td = c(firstline ? "th" : "td");
         tr.appendChild(td);
         td.textContent = s3;
       }
       tbl.appendChild(tr);
+      firstline = false;
     }
     this.appendChild(tbl);
 
     // css
-    const style = document.createElement('style');
+    const style = c('style');
     style.textContent = `
       table {
         border-collapse: collapse;
       }
-      td {
+      td, th {
         border: 1px solid #333;
         padding: .1em .5em;
         font-size: 90%;
