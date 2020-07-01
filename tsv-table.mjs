5d69a9669799b9c4e96138fe351b547b1adc4360
Taisuke Fukuno <fukuno@jig.jp>
2020-07-01 15:34:58 +0900

    use decodeTSV

--- tsv-table.mjs
+++ tsv-table.mjs
@@ -1,7 +1,74 @@
+const decodeTSV = (s) => {
+  const sepc = "\t";
+  const res = [];
+  let st = 0;
+  let line = [];
+  let sb = null;
+  if (!s.endsWith("\n")) { s += "\n"; }
+  const len = s.length;
+  for (let i = 0; i < len; i++) {
+    const c = s.charAt(i);
+    if (c === "\r") { continue; }
+    if (st === 0) {
+      if (c === "\n") {
+        if (line.length > 0) { line.push(""); }
+        res.push(line);
+        line = [];
+      } else if (c == sepc) {
+        line.push("")
+      } else if (c == "\"") {
+        sb = "";
+        st = 2;
+      } else {
+        sb = c;
+        st = 1;
+      }
+    } else if (st === 1) {
+      if (c === "\n") {
+        line.push(sb);
+        res.push(line);
+        line = [];
+        st = 0;
+        sb = null;
+      } else if (c === sepc) {
+        line.push(sb);
+        sb = null;
+        st = 0;
+      } else {
+        sb += c;
+      }
+    } else if (st === 2) {
+      if (c === "\"") {
+        st = 3;
+      } else {
+        sb += c;
+      }
+    } else if (st === 3) {
+      if (c === "\"") {
+        sb += c;
+        st = 2;
+      } else if (c === sepc) {
+        line.push(sb);
+        sb = null;
+        st = 0;
+      } else if (c === "\n") {
+        line.push(sb);
+        res.push(line);
+        line = [];
+        st = 0;
+        sb = null;
+      }
+    }
+  }
+  if (sb != null) { line.push(sb); }
+  if (line.length > 0) { res.push(line); }
+  return res;
+};
+
 class TSVTable extends HTMLElement {
   constructor () {
     super();
-    const ss = this.textContent.trim().split("\n");
+    const ss = decodeTSV(this.textContent.trim());
     this.textContent = "";
 
     const c = tag => document.createElement(tag);
@@ -10,8 +77,7 @@ class TSVTable extends HTMLElement {
     for (const s of ss) {
       const tr = c("tr");
       tbl.append(tr);
-      const s2 = s.split("\t");
-      for (const s3 of s2) {
+      for (const s3 of s) {
         const td = c(firstline ? "th" : "td");
         tr.appendChild(td);
         td.textContent = s3;
@@ -22,7 +88,7 @@ class TSVTable extends HTMLElement {
     this.appendChild(tbl);
 
     // css
-    const style = c('style');
+    const style = c("style");
     style.textContent = `
       table {
         border-collapse: collapse;
@@ -37,4 +103,4 @@ class TSVTable extends HTMLElement {
   }
 }
 
-customElements.define('tsv-table', TSVTable);
+customElements.define("tsv-table", TSVTable);
